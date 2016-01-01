f7478ba265053f5454e7413c9dca4da906aa6824
joshsoup <joshua.jacobs@my.und.edu>
2015-12-31 22:45:30 -0600

    Added character cpp diggity dog!

---
+++ Character.cpp
@@ -0,0 +1,22 @@
+/*
+ * Character.cpp
+ *
+ *  Created on: Nov 8, 2014
+ *      Author: Josh
+ */
+
+#include "Character.h"
+
+
+Character::Character(int inX, int inY)
+{
+	x = inX;
+	y = inY;
+	// TODO Find tile and initialize its contents.
+
+}
+
+Character::~Character() {
+	// TODO Auto-generated destructor stub
+}
+
